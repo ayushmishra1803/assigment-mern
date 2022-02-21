876ba0c1c4d5534ec3e19ab4f69dc5ce36d14c08
ayushmishra18013 <ayushmishra0810@gmail.com>
2022-02-22 02:17:56 +0530

    inital commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://ayush1803:ayush1803@cluster0.aqrwa.mongodb.net/E-commerse?retryWrites=true&w=majority`
+);
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Pages/login/Login";
+import SignUp from "./Pages/sign-up/Sign-up";
+
+function App() {
+  return (
+    <div>
+      <Login />{" "}
+    </div>
+  );
+}
+
+export default App;
